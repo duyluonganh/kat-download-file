eba812e142e25241fea00deaf8195aeb2e531ce6
Duy Luong <duy.luong@katalon.com>
2021-06-30 11:54:03 +0700

    Update

---
+++ Test Suites/New Test Suite (1).ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (1)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1407ced9-fb86-421e-93b1-a82d530697be</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca1bc6c5-7a11-46ae-ad14-1493762169c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Failed Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
